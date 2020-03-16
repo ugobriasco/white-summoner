91cdcd6efd02930b05ee4e6e9b5c542464ced050
Ugo Briasco <ugo.briasco@sumup.com>
2020-03-16 18:15:11 +0100

    Add CL company registration number

--- tests/integration/public.spec.js
+++ tests/integration/public.spec.js
@@ -1,43 +1,43 @@
-const exec = require("child_process").exec;
-const chai = require("chai");
+const exec = require('child_process').exec;
+const chai = require('chai');
 chai.should();
 
 // Test Data
-const { countriesAvailable } = require("../../lib/data");
+const { countriesAvailable } = require('../../lib/data');
 
 // Methods under test
-const getProfile = require("../../../white-summoner");
+const getProfile = require('../../../white-summoner');
 
 before(() => {
-  exec("npm run build:deptree");
+  exec('npm run build:deptree');
   const currentPath = process.cwd();
   console.log(
-    "\x1b[36m%s\x1b[0m",
+    '\x1b[36m%s\x1b[0m',
     `--> Dependency tree available under ${currentPath}/dependencygraph.svg \n\n`
   );
 });
 
 // Test sets
-describe("Public methods", () => {
+describe('Public methods', () => {
   // Core usage
-  describe("lib/getProfile", () => {
-    it("should resolve a promise", done => {
+  describe('lib/getProfile', () => {
+    it('should resolve a promise', done => {
       getProfile({
-        countryCode: "DE"
+        countryCode: 'DE'
       }).then(data => {
-        data.should.be.a("object");
+        data.should.be.a('object');
         done();
       });
     });
-    it("should return a user with a valid country code", done => {
-      getProfile({ countryCode: "DE" }).then(data => {
-        data.country.should.be.equal("DE");
+    it('should return a user with a valid country code', done => {
+      getProfile({ countryCode: 'DE' }).then(data => {
+        data.country.should.be.equal('DE');
         done();
       });
     });
 
     // Localization
-    describe("Coverage of available countries", () => {
+    describe('Coverage of available countries', () => {
       countriesAvailable.forEach(country => {
         it(`covers ${country}`, done => {
           getProfile({ countryCode: country }).then(data => {
@@ -45,11 +45,11 @@ describe("Public methods", () => {
             data.address.country.should.be.equal(country);
             data.address.country.should.be.equal(country);
             if (
-              country == "JP" ||
-              country == "DE" ||
-              country == "US" ||
-              country == "CL" ||
-              country == "CO"
+              country == 'JP' ||
+              country == 'DE' ||
+              country == 'US' ||
+              country == 'CL' ||
+              country == 'BR'
             ) {
               console.log(data);
             }
